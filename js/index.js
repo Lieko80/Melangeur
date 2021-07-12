0775004571005893068c92f7c695ffdec8739bab
Fabien Trouillet <trouillet.fabien@yahoo.com>
2021-07-12 16:14:40 +0200

    prend plusieur phrase en les separant bien

--- js/index.js
+++ js/index.js
@@ -6,32 +6,57 @@ var check = document.getElementById("envoi");
 check.addEventListener("click", function verif(event) {
 //variables 
 var phrase = document.getElementById("phrase").value;
+var splitphrase;
 var split;
+var longphrase;
 var long;
 var result;
     
-    //separe les mot par un espaces
-    split = phrase.split(' ');
-    //les met par ordre alphabetique    
-    split.sort();
-    console.log("mis alpha" + split);
+    //separe les phrase
+    splitphrase = phrase.split('.');
 
-    //recupere la taille du tableau
-    long = split.length;
-    console.log("longueur" + long);
+    // randomizer tableau
+    function shuffleArray(inputArray){
+        inputArray.sort(()=> Math.random() - 0.5);
+    }
 
-    for(i = 0; i<long; i++){
-        //j'enleve le premier mot du tableau a cahque tour de boucle
-        result = split.shift();
-        console.log(result);
+    //longueur des phrases
+    longphrase = splitphrase.length - 1;
+    console.log("nombre de phrase : " + longphrase);    
 
-        //je creer des boutons en leur mettant les valeur de split en allant
-        var btn = document.createElement("BUTTON");      
-        btn.setAttribute("class", "btn btn-outline-primary");                                
-        btn.innerHTML = result;                
-        document.getElementById("cont").appendChild(btn);
+    for (j = 0; j<longphrase; j++){
+        split = splitphrase[j].split(" ");
+        console.log("phrase spliter : " + split);
 
-    }   
+        //melange les mots
+        shuffleArray(split);
+        console.log("randomiser melanger : " + split);
 
+        //recupere la taille du tableau
+        long = split.length;
+        console.log("nombre de mots dans la phrase : " + long);
+
+        //je creer une div pour chaque phrase
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "container");
+
+        for(i = 0; i<long; i++){
+            //j'enleve le premier mot du tableau a cahque tour de boucle
+            result = split.shift();
+            console.log("resultat : " + result);
+
+            //je creer des boutons en leur mettant les valeur de split en allant
+            var btn = document.createElement("BUTTON");  
+            btn.setAttribute("class", "btn btn-outline-secondary");                                
+            btn.innerHTML = result;                
+            
+            newDiv.appendChild(btn);
+        }   
+
+        // ajoute le nouvel élément créé et son contenu dans le DOM
+        var currentDiv = document.getElementById(i);
+        document.body.insertBefore(newDiv, currentDiv);
+
+    }
 
 });
